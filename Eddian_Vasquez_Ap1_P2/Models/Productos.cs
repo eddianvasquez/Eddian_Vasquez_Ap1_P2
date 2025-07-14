9c9f71ba860e51f45fd5a20eae49ecfd2ce4fe6e
eddianvasquez <eddian_vasquez@ucne.edu.do>
2025-07-14 19:19:19 -0400

    agregando Productos

---
+++ Eddian_Vasquez_Ap1_P2/Models/Productos.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Eddian_Vasquez_Ap1_p2.Models
+{
+    public class Productos
+    {
+        [Key]
+        public int ProductoId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nombre { get; set; } = string.Empty;
+
+        [Required]
+        [Range(0, double.MaxValue, ErrorMessage = "El peso debe ser mayor a cero")]
+        public double Peso { get; set; }
+    }
+}
+
