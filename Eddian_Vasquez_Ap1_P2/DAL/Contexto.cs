fe064d0f2e0898362581918ad67493033c413b4b
eddianvasquez <eddian_vasquez@ucne.edu.do>
2025-07-14 19:23:12 -0400

    Agregando Contexto

--- Eddian_Vasquez_Ap1_P2/DAL/Contexto.cs
+++ Eddian_Vasquez_Ap1_P2/DAL/Contexto.cs
@@ -1,12 +1,29 @@
 ﻿using System.Collections.Generic;
-using System.Reflection.Emit;
-
 using Microsoft.EntityFrameworkCore;
-using Eddian_Vasquez_Ap1_P2.Models;
-namespace Eddian_Vasquez_Ap1_P2
-{
+using Eddian_Vasquez_Ap1_p2.Models;
+using static Eddian_Vasquez_Ap1_P2.Components.Pages.VentasPages.Index;
 
+namespace Re.Context
+{
     public class Contexto : DbContext
     {
         public Contexto(DbContextOptions<Contexto> options) : base(options) { }
 
+        public DbSet<Productos> Productos { get; set; }
+        public DbSet<Detalle> Detalles { get; set; }
+        public DbSet<Entradas> Entradas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Seed de productos
+            modelBuilder.Entity<Productos>().HasData(new List<Productos>
+            {
+                new Productos { ProductoId = 1, Nombre = "Maní", Peso = 5 },
+                new Productos { ProductoId = 2, Nombre = "Almendra", Peso = 10 },
+                new Productos { ProductoId = 3, Nombre = "Pistacho", Peso = 15 }
+            });
+        }
+    }
+}
