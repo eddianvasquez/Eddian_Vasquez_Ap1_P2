497c5215b44b70096e9c5659fcc9a5ac6fc4b188
eddianvasquez <eddian_vasquez@ucne.edu.do>
2025-07-14 19:14:33 -0400

    agregando contexto

--- Eddian_Vasquez_Ap1_P2/DAL/Contexto.cs
+++ Eddian_Vasquez_Ap1_P2/DAL/Contexto.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
 
-namespace Eddian_Vasquez_Ap1_P2.DAL
+using Microsoft.EntityFrameworkCore;
+using Eddian_Vasquez_Ap1_P2.Models;
+namespace Eddian_Vasquez_Ap1_P2
 {
-    public class Contexto: DbContext
+
+    public class Contexto : DbContext
     {
-    }
-}
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
