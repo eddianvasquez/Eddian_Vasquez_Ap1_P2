542c2b1adc4d0f5155daa8f8ce2b2c12c17de73c
eddianvasquez <eddian_vasquez@ucne.edu.do>
2025-07-14 19:14:07 -0400

    agregando detalle

--- Eddian_Vasquez_Ap1_P2/Models/Detalle.cs
+++ Eddian_Vasquez_Ap1_P2/Models/Detalle.cs
@@ -1,10 +1,29 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
-namespace Eddian_Vasquez_Ap1_P2.Models
+namespace Eddian_Vasquez_Ap1_p2.Models
 {
-    public class Detalle
+    public class Entradas
     {
         [Key]
-        public int Id { get; set; }
+        public int EntradaId { get; set; }
+
+        [Required]
+        public DateTime Fecha { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Concepto { get; set; } = string.Empty;
+
+        [Required]
+        [Range(0, double.MaxValue, ErrorMessage = "El peso debe ser positivo")]
+        public double PesoTotal { get; set; }
+
+        [Required]
+        public int IdProducido { get; set; }
+
+        [Required]
+        [Range(0, double.MaxValue, ErrorMessage = "La cantidad debe ser positiva")]
+        public double CantidadProducida { get; set; }
     }
 }
