85a9270383f81222082d514a65ef6b5693e8b70c
eddianvasquez <eddian_vasquez@ucne.edu.do>
2025-07-15 00:37:00 -0400

    agregando componente

--- Eddian_Vasquez_Ap1_P2/DAL/Contexto.cs
+++ Eddian_Vasquez_Ap1_P2/DAL/Contexto.cs
@@ -1,54 +1,70 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Eddian_Vasquez_Ap1_P2.Models;
-using Eddian_Vasquez_Ap1_p2.Models;
+﻿using Eddian_Vasquez_Ap1_p2.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
-namespace Eddian_Vasquez_Ap1_P2.Data 
+namespace Eddian_Vasquez_Ap1_p2.Context
 {
     public class Contexto : DbContext
     {
         public Contexto(DbContextOptions<Contexto> options) : base(options) { }
 
         public DbSet<Entrada> Entradas { get; set; }
-        public DbSet<EntradaDetalle> EntradaDetalles { get; set; }
+        public DbSet<EntradaDetalle> EntradasDetalle { get; set; }
         public DbSet<Producto> Productos { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
-          
-            modelBuilder.Entity<Entrada>()
-                .HasMany(e => e.Detalles)
-                .WithOne(d => d.Entrada)
-                .HasForeignKey(d => d.EntradaId)
-                .OnDelete(DeleteBehavior.Cascade);
-
-           
-            modelBuilder.Entity<Producto>().HasData(new List<Producto>
+            modelBuilder.Entity<Producto>().HasData(new List<Producto>()
             {
                 new Producto
                 {
                     ProductoId = 1,
                     Descripcion = "Maní",
-                    Peso = 5.0,
-                    Existencia = 0,
+                    Peso = 0,
+                    Existencia = 100,
                     EsCompuesto = false
                 },
                 new Producto
                 {
                     ProductoId = 2,
-                    Descripcion = "Pistacho",
-                    Peso = 10.0,
-                    Existencia = 0,
+                    Descripcion = "Pistachos",
+                    Peso = 0,
+                    Existencia = 100,
                     EsCompuesto = false
                 },
                 new Producto
                 {
                     ProductoId = 3,
-                    Descripcion = "Almendra",
-                    Peso = 15.0,
-                    Existencia = 0,
+                    Descripcion = "Almendras",
+                    Peso = 0,
+                    Existencia = 100,
                     EsCompuesto = false
+                },
+                new Producto
+                {
+                    ProductoId = 4,
+                    Descripcion = "Frutos Mixtos 200gr",
+                    Peso = 200.00,
+                    Existencia = 0,
+                    EsCompuesto = true
+                },
+                new Producto
+                {
+                    ProductoId = 5,
+                    Descripcion = "Frutos Mixtos 400gr",
+                    Peso = 400.00,
+                    Existencia = 0,
+                    EsCompuesto = true
+                },
+                new Producto
+                {
+                    ProductoId = 6,
+                    Descripcion = "Frutos Mixtos 600gr",
+                    Peso = 600.00,
+                    Existencia = 0,
+                    EsCompuesto = true
                 }
             });
         }
--- Eddian_Vasquez_Ap1_P2/DAL/ContextoFactory.cs
+++ Eddian_Vasquez_Ap1_P2/DAL/ContextoFactory.cs
@@ -1,15 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
-using Eddian_Vasquez_Ap1_P2.Data; 
+using Eddian_Vasquez_Ap1_p2.Context;
 
-public class ContextoFactory : IDesignTimeDbContextFactory<Contexto>
+
+namespace Eddian_Vasquez_Ap1_p2.Context
 {
-    public Contexto CreateDbContext(string[] args)
+    public class ContextoFactory : IDesignTimeDbContextFactory<Contexto>
     {
-        var optionsBuilder = new DbContextOptionsBuilder<Contexto>();
+        public Contexto CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<Contexto>();
+            optionsBuilder.UseSqlServer("workstation id=vamosplis.mssql.somee.com;packet size=4096;user id=pollito_SQLLogin_1;pwd=6jmfstvuoj;data source=vamosplis.mssql.somee.com;persist security info=False;initial catalog=vamosplis;TrustServerCertificate=True");
 
-        optionsBuilder.UseSqlServer("workstation id=vamospapa.mssql.somee.com;packet size=4096;user id=pollito_SQLLogin_1;pwd=6jmfstvuoj;data source=vamospapa.mssql.somee.com;persist security info=False;initial catalog=vamospapa;TrustServerCertificate=True");
-
-        return new Contexto(optionsBuilder.Options);
+            return new Contexto(optionsBuilder.Options);
+        }
     }
 }
